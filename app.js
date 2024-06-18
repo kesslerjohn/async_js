67bebc28f6b4f21ad544161219b764ab39d4d4e0
kesslerjohn <john-kessler@uiowa.edu>
2024-06-18 17:34:38 -0500

    start ice cream production example

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-// synchronous
-console.log("I")
-console.log("eat")
+let stocks = {
+    Fruits : ["strawberries", "grapes", "bananas", "apples"],
+    Liquid : ["water", "ice"],
+    Holder : ["cone", "cup", "stick"],
+    Toppings : ["chocolate", "peanuts"]
+}
 
-setTimeout(() => {console.log("ice cream")}, 3000)
 
-console.log("with")
-console.log("a spoon")
+
+let order = (fruit_idx, prod_fn) => {
+    setTimeout(() => {
+        console.log(`${stocks.Fruits[fruit_idx]}`)
+    }, 2000)
+
+    prod_fn()
+}
+
+let production = () => {
+    
+}
+
+order(0, production)
