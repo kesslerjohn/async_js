272fee5d8cab010b509d2a3cc0f7b24b890f8d07
kesslerjohn <john-kessler@uiowa.edu>
2024-06-22 15:56:23 -0500

    first practice with await

--- app.js
+++ app.js
@@ -7,24 +7,29 @@ let stocks = {
 
 let shopIsOpen = true
 
-/*let order = () => {
-    return new Promise( (resolve, reject) => {
-
-    })
-}*/
-
 let toppingsChoice = () => {
     return new Promise( (resolve, reject) => {
         setTimeout( () => {
+            resolve(
             console.log("which topping would you like?")
-        }, 3000)
+        )}, 3000)
     })
 }
 
 async function kitchen() {
     console.log("A")
     console.log("B")
+    await toppingsChoice()
+
     console.log("C")
     console.log("D")
     console.log("E")
 }
+
+kitchen()
+
+console.log("doing dishes")
+
+console.log("cleaning tables")
+
+console.log("other tasks")
